e6d4cd46a075864c608161b5c11b2743aa960eb4
David Moravec <david.moravec@gmail.com>
2026-07-13 11:16:38 +0200

    Typo storage

--- resources.tf
+++ resources.tf
@@ -11,7 +11,7 @@ resource "azurerm_storage_account" "web_storage" {
   name                     = "dmpltfwebsiteapp2026" # Must be globally unique, lowercase alphanumeric only
   resource_group_name      = azurerm_resource_group.infra_rg.name
   location                 = azurerm_resource_group.infra_rg.location
-  account_kind             = "StandardV2"
+  account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
   min_tls_version          = "TLS1_2"
