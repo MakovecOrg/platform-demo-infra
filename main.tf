cdfacf15a45431295a861d9ac32f17a85bc0fb86
David Moravec <david.moravec@gmail.com>
2026-07-13 11:13:59 +0200

    Changes to create web

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 terraform {
+  required_version = ">=1.15"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.9"
     }
   }
 
