091e1ee31c3c08e9d983cc4713b0dfcf6087a9d7
David Moravec <david.moravec@gmail.com>
2026-07-13 13:10:43 +0200

    Outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@ output "website_url" {
   value       = azurerm_storage_account.web_storage.primary_web_endpoint
   description = "The public web URL of your static hosted site."
 }
+
+output "sa" {
+  value       = azurerm_storage_account.web_storage.id
+  description = "Storage account"
+}
+
+output "sa_cont" {
+  value       = azurerm_storage_container.web_container.name
+  description = "Container"
+}
+
+output "sa_cont_addr" {
+  value       = azurerm_storage_blob.index_file.name
+  description = "Web root"
+}
