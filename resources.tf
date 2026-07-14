cee93d563661b1d7c803a5d55fb08ee1eb293d2e
David Moravec <david.moravec@gmail.com>
2026-07-14 10:50:56 +0200

    Removed web resources after test

--- resources.tf
+++ resources.tf
@@ -22,35 +22,3 @@ resource "azurerm_storage_account_static_website" "web" {
   index_document     = "index.html"
 }
 
-resource "local_file" "index" {
-  filename = "index.html"
-  content  = <<EOF
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Deployed via Terraform</title>
-</head>
-<body>
-    <h1>Deployment Successful!</h1>
-    <p><strong>Commit ID:</strong> ${var.commit_id} </p>
-</body>
-</html>
-EOF
-}
-
-data "azurerm_storage_container" "web_container" {
-  name                 = "$web"
-  storage_account_name = azurerm_storage_account.web_storage.name
-}
-
-resource "azurerm_storage_blob" "index_file" {
-  name                   = "index.html"
-  storage_account_name   = azurerm_storage_account.web_storage.name
-  storage_container_name = data.azurerm_storage_container.web_container.name
-  type                   = "Block"
-  content_type           = "text/html"
-  source                 = "index.html"
-
-  depends_on = [local_file.index]
-
-}
